75d46d0d5e12cb37e4dc98188c7bd6185acf90fd
ggzzggzzz <ziobang77@hs.ac.kr>
2025-06-20 15:28:12 +0900

    create

---
+++ src/main/java/kr/ac/kopo/home/controller/HomeController.java
@@ -0,0 +1,12 @@
+package kr.ac.kopo.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index"; // index.jsp 로 이동
+    }
+}
